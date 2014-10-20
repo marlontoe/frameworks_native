70acc9abbdcedfc8bc655a2e7491c56e1f6f826f
marlontoe <inclusionrec@googlemail.com>
2014-10-21 00:38:56 +0200

    code fixup

--- include/binder/Parcel.h
+++ include/binder/Parcel.h
@@ -27,12 +27,8 @@
 // ---------------------------------------------------------------------------
 namespace android {
 
-template <typename T> class LightFlattenable;
-#ifdef STE_HARDWARE
-class Flattenable;
-#else
 template <typename T> class Flattenable;
-#endif
+template <typename T> class LightFlattenable;
 class IBinder;
 class IPCThreadState;
 class ProcessState;
@@ -102,18 +98,14 @@ public:
     status_t            writeCString(const char* str);
     status_t            writeString8(const String8& str);
     status_t            writeString16(const String16& str);
-    status_t            writeString16(const uint16_t* str, size_t len);
+    status_t            writeString16(const char16_t* str, size_t len);
     status_t            writeStrongBinder(const sp<IBinder>& val);
     status_t            writeWeakBinder(const wp<IBinder>& val);
     status_t            writeInt32Array(size_t len, const int32_t *val);
     status_t            writeByteArray(size_t len, const uint8_t *val);
-#ifdef STE_HARDWARE
-    status_t            write(const Flattenable& val);
-#else
+
     template<typename T>
     status_t            write(const Flattenable<T>& val);
-#endif
-
 
     template<typename T>
     status_t            write(const LightFlattenable<T>& val);
@@ -168,12 +160,9 @@ public:
     const char16_t*     readString16Inplace(size_t* outLen) const;
     sp<IBinder>         readStrongBinder() const;
     wp<IBinder>         readWeakBinder() const;
-#ifdef STE_HARDWARE
-    status_t            read(Flattenable& val) const;
-#else
+
     template<typename T>
     status_t            read(Flattenable<T>& val) const;
-#endif
 
     template<typename T>
     status_t            read(LightFlattenable<T>& val) const;
@@ -286,18 +275,11 @@ private:
         virtual status_t unflatten(void const* buffer, size_t size, int const* fds, size_t count) = 0;
     };
 
-#ifndef STE_HARDWARE
     template<typename T>
-#endif
     class FlattenableHelper : public FlattenableHelperInterface {
         friend class Parcel;
-#ifdef STE_HARDWARE
-        const Flattenable& val;
-        explicit FlattenableHelper(const Flattenable& val) : val(val) { }
-#else
         const Flattenable<T>& val;
         explicit FlattenableHelper(const Flattenable<T>& val) : val(val) { }
-#endif
 
     public:
         virtual size_t getFlattenedSize() const {
@@ -310,11 +292,7 @@ private:
             return val.flatten(buffer, size, fds, count);
         }
         virtual status_t unflatten(void const* buffer, size_t size, int const* fds, size_t count) {
-#ifdef STE_HARDWARE
-            return const_cast<Flattenable&>(val).unflatten(buffer, size, fds, count);
-#else
             return const_cast<Flattenable<T>&>(val).unflatten(buffer, size, fds, count);
-#endif
         }
     };
     status_t write(const FlattenableHelperInterface& val);
@@ -336,13 +314,11 @@ public:
 
 // ---------------------------------------------------------------------------
 
-#ifndef STE_HARDWARE
 template<typename T>
 status_t Parcel::write(const Flattenable<T>& val) {
     const FlattenableHelper<T> helper(val);
     return write(helper);
 }
-#endif
 
 template<typename T>
 status_t Parcel::write(const LightFlattenable<T>& val) {
@@ -362,13 +338,11 @@ status_t Parcel::write(const LightFlattenable<T>& val) {
     return NO_ERROR;
 }
 
-#ifndef STE_HARDWARE
 template<typename T>
 status_t Parcel::read(Flattenable<T>& val) const {
     FlattenableHelper<T> helper(val);
     return read(helper);
 }
-#endif
 
 template<typename T>
 status_t Parcel::read(LightFlattenable<T>& val) const {
